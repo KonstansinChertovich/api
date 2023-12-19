71932c212a41717facddb7d81e51cbac096785c7
Konstantsin <shulghin1991@mail.ru>
2023-12-19 12:49:13 +0300

    add data in client-modal

--- models/client-model.js
+++ models/client-model.js
@@ -5,7 +5,12 @@ const ClientSchema = new Schema({
     phone: {type: String},
     service: {type: String},
     connection: {type: String},
-    text: {type: String}
+    text: {type: String},
+    date: {type: Object({
+        day: {type: String},
+        time: {type: String}
+    })},
+    firstOpen: {type: Boolean, default: false}
 })
 
 module.exports = model('Client', ClientSchema)
