f867b7eb7baa8d1704f35d172eb0e0b700c462e9
Konstantsin <shulghin1991@mail.ru>
2024-01-09 17:52:25 +0300

    add url origin cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: ["https://admin-project-psycholog.netlify.app", "https://admin-project-psycholog.netlify.app"],
+    origin: ["https://admin-project-psycholog.netlify.app", "https://project-psycholog.netlify.app"],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 app.use('/api', router)
